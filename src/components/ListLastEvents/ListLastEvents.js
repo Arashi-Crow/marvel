e6dd0b9cdaf93a3a1a0edc07531653b2cc12bf4f
Arashi-Crow <ignaciofdce92@gmail.com>
2024-04-21 17:33:18 -0600

    api working

--- src/components/ListLastEvents/ListLastEvents.js
+++ src/components/ListLastEvents/ListLastEvents.js
@@ -1,15 +1,13 @@
 import { Card, Header } from "semantic-ui-react";
 import Container from "../Container";
 import "./ListLastEvents.scss";
-// import useFetch from "../../hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 import LastEvents from "../LastEvents/LastEvents";
 
 export default function ListLastEvents() {
-    const lastEventsFetch = "";
-//   const lastEventsFetch = useFetch(
-//     `${process.env.REACT_APP_URL_BASE}/events?ts=1&apikey=${process.env.REACT_APP_API_KEY_MARVEL}&hash=${process.env.REACT_APP_HASH_ID}&orderBy=startDate&limit=5`
-//   );
-    console.log(process.env.REACT_APP_URL_BASE);
+  const lastEventsFetch = useFetch(
+    `${process.env.REACT_APP_URL_BASE}/events?ts=1&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_ID}&orderBy=startDate&limit=5`
+  );
 
   return (
     <div className="container-list-last-events">
